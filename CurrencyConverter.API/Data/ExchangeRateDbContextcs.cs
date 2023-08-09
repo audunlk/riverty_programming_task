1598a295310f26cb4a24a0def3804d1bfa706f45
audunlk <audunlk@gmail.com>
2023-08-09 14:53:15 +0200

    task 1c

---
+++ CurrencyConverter.API/Data/ExchangeRateDbContextcs.cs
@@ -0,0 +1,15 @@
+﻿using CurrencyConverter.ConsoleApp.Models;
+using Microsoft.EntityFrameworkCore;
+namespace CurrencyConverter.API.Data
+{
+    public class ExchangeRateDbContextcs
+    {
+        public class ExchangeRateDbContext : DbContext
+        {
+            public ExchangeRateDbContext(DbContextOptions<ExchangeRateDbContext> options) : base(options)
+            {
+            }
+            public DbSet<Response> ExchangeRates { get; set; }
+        }
+    }
+}
